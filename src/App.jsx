a4b2d0bedcc0c67c58da21eb63fb55d3c19db57c
Bharat Yadav <bharatyadavjws@gmail.com>
2023-10-31 21:04:06 +0530

    emi calculator added

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Navbar from './components/navbar/Navbar'
 import Footer from './components/footer/Footer'
 import Testimonials from './components/testimonials/Testimonials'
 import Faquestions from './components/faquestions/Faquestions'
+import Emicalculator from './components/emicalculator/Emicalculator'
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
         <Applynow/>
         <WhyRightrate/>
         <Eligibility/>
+        <Emicalculator/>
         <Testimonials/>
         <Faquestions/>
         <Footer/>
