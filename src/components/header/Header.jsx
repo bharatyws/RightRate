639a91c2d0ad346a6d0769f56313b1e1abcfd8f6
Bharat Yadav <bharatyadavjws@gmail.com>
2023-11-09 13:54:47 +0530

    header error resolved

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,27 +5,30 @@ import HeadImg from '../../assets/head_graphic.png'
 const Header = () => {
   return (
     <header className='container header__container'>
+        <div className="main__container">
         <section className="header header__text">
             <div className="head__content">
-            <div className="main__text">
-                <h2>Go Beyond Ordinary</h2>
-                <h1>Banking</h1>
-            </div>
-            <div className='extras'>
-                <p>Get Upto <span>₹50000</span> in Just 3 Minutes</p>
-                <a href="#" className='head__apply btn btn-primary'>Apply Now</a>
-                <div className='powered__by'>
-                    <p>Powered by RightRate & Trusted by</p>
-                    <h3 className='trusted__number'><span>15000+</span> Individuals</h3>
+                <div className="main__text">
+                    <h2>Go Beyond Ordinary</h2>
+                    <h1>Banking</h1>
                 </div>
-            </div>
+                <div className='extras'>
+                    <p>Get Upto <span>₹50000</span> in Just 3 Minutes</p>
+                    <a href="/" className='head__apply btn btn-primary'>Apply Now</a>
+                    <div className='powered__by'>
+                        <p>Powered by RightRate & Trusted by</p>
+                        <h3 className='trusted__number'><span>15000+</span> Individuals</h3>
+                    </div>
+                </div>
             </div>
             
         </section>
 
         <section className="header header__graphics">
             <img src={HeadImg} alt='Head_carousal'/>
-        </section>      
+        </section> 
+        </div>
+             
     </header>
   )
 }
